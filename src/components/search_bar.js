6ea1c8f61c6f82ea932ea9203f07feb6e1b6b53f
xants <kairey@gmail.com>
2017-10-22 15:18:55 -0700

    search bar edit

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-  //class based component
   constructor(props) {
     super(props);
+
     this.state = { term: "" };
   }
+
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })}
+          onChange={event => this.onInputChange(event.target.value)}
         />
       </div>
-    ); //on + Event = +eventHandler
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
 }
 
