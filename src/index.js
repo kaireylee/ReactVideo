7508c3dffe89670e4e7942085f5a32b0388cfa13
xants <kairey@gmail.com>
2017-10-23 15:20:24 -0700

    index edit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
-
-const API_KEY = "AIzaSyDJAQI5X4Ox-xe4q2E7i2dm3iZgvDyNmwM";
+const API_KEY = "AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
-      this.setState({ videos });
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
       <div>
-        <SearchBar />
-        <VideoDetail videos={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.querySelector(".container")); //es6 javascript modules
+ReactDOM.render(<App />, document.querySelector(".container"));
