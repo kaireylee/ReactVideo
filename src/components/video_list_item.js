32663caddf12d3783dee6327dbf6e302564ffda3
Kai-Rey Lee <kairey@gmail.com>
2017-12-16 13:40:33 -0800

    video list item fixed

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const VideoListItem = ({video}) => {
-  return <li>Video</li>;
+const VideoListItem = ({ video }) => {
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div classname="media-left">
+          <img className="media-object" />
+        </div>
+
+        <div className="media-body">
+          <div classname="media-heading" />
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
