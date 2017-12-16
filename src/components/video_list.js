c4807289216715f04fca234cfe8d9731fe2d42ab
Kai-Rey Lee <kairey@gmail.com>
2017-12-16 01:16:42 -0800

    video list item added

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,7 +1,11 @@
 import React from "react";
+import VideoListItem from "./video_list_item";
 
 const VideoList = props => {
-  return <ul className="col-md-4 list-group">{props.videos.length}</ul>;
+  const videoItems = props.videos.map(videos => {
+    return <VideoListItem video={video} />;
+  });
+  return <ul className="col-md-4 list-group">{videoItems}</ul>;
 };
 
 export default VideoList;
